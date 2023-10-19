4c393bfb9d768ee3589ddacdb787cd1c48c1c2b8
Rishabh Gupta <3rishabh3@gmail.com>
2023-10-19 18:20:03 +0530

    setup controllers, middlewares, and routes for home

--- backend/app.js
+++ backend/app.js
@@ -3,5 +3,11 @@ const app = express();
 
 require("dotenv").config();
 
+// import all routes
+const home = require("./routes/home");
+
+// router middleware
+app.use("/api/v1", home);
+
 // export app js
 module.exports = app;
