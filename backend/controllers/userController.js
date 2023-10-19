5d9c02c7d879a3faa967d57770262774df3f794b
Rishabh Gupta <3rishabh3@gmail.com>
2023-10-19 19:15:28 +0530

    created user-login endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,3 +22,31 @@ exports.signup = BigPromise(async (req, res, next) => {
 
   cookieToken(user, res);
 });
+
+exports.login = BigPromise(async (req, res, next) => {
+  const { phone, password } = req.body;
+
+  // check if phone or password is missing
+  if (!phone || !password) {
+    return next(new CustomError("Phone and Password both are required", 400));
+  }
+
+  // get user from db
+  const user = await User.findOne({ phone }).select("+password");
+
+  // if user not found in db
+  if (!user) {
+    return next(new CustomError("Phone is not registered", 400));
+  }
+
+  // match the password
+  const isValidPassword = await user.isValidPassword(password);
+
+  // if password do not match
+  if (!isValidPassword) {
+    return next(new CustomError("Password is not correct", 400));
+  }
+
+  // if everything is fine then generate token
+  cookieToken(user, res);
+});
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { signup } = require("../controllers/userController");
+const { signup, login } = require("../controllers/userController");
 
 router.route("/add-user").post(signup);
+router.route("/login-user").post(login);
 
 module.exports = router;
